5a69267469c298ff6a7867ade118bb2bba671c82
Carlos Melo <carlos@rematter.com>
2024-09-15 22:27:32 -0300

    fix formula att 1

--- Formula/qt-ads.rb
+++ Formula/qt-ads.rb
@@ -1,8 +1,11 @@
 class QtAds < Formula
   desc "A docking system for Qt" 
   homepage "https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System"
-  url "https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System/archive/refs/tags/4.3.1.tar.gz"
-  sha256 "d7c244d9accaa78766e90124c0ce6054327b78625dae8ba2cfe22fc29dfba37d"
+  url "https://github.com/githubuser0xFFFF/Qt-Advanced-Docking-System.git",
+      using: :git,
+      tag: "4.3.1" 
+  version "4.3.1"
+  license "LGPL v2"
 
   depends_on "cmake" => :build
   depends_on "qt"
